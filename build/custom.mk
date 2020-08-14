1ab0cbf24721dca08ac18ca05d82e8952a2126f3
asimsedhain <asimsedhain33@gmail.com>
2020-08-14 01:16:53 -0500

    fix: make memelibrary

--- build/custom.mk
+++ build/custom.mk
@@ -1,1 +1,7 @@
-# Include custome targets and environment variables here
+# Include custom targets and environment variables here
+
+.DEFAULT_GOAL := all
+
+memelibrary: $(shell find server/memelibrary/assets)
+	env GO111MODULE=off $(GO) get github.com/jteeuwen/go-bindata/...
+	$(GOPATH)/bin/go-bindata -o server/memelibrary/assets.go -pkg memelibrary -prefix server/memelibrary/assets/ -ignore '(^|/)\..*' server/memelibrary/assets/...
