f215e47e95cff86cab86e63930d4bc3151b98f51
asimsedhain <asimsedhain33@gmail.com>
2020-08-31 10:57:13 -0500

    refactor: move GetSiteURL function

--- server/plugin.go
+++ server/plugin.go
@@ -154,15 +154,6 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 	p.router.ServeHTTP(w, r)
 }
 
-func (p *Plugin) GetSiteURL() string {
-	siteURL := ""
-	ptr := p.API.GetConfig().ServiceSettings.SiteURL
-	if ptr != nil {
-		siteURL = *ptr
-	}
-	return siteURL
-}
-
 func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
 	siteURL := p.GetSiteURL()
 
@@ -210,6 +201,15 @@ Available memes: ` + strings.Join(availableMemes, ", "),
 		Text:         "![" + template.Name + "](" + siteURL + "/plugins/memes/templates/" + template.Name + ".jpg" + queryString + ")",
 	}
 	return resp, nil
+}
+
+func (p *Plugin) GetSiteURL() string {
+	siteURL := ""
+	ptr := p.API.GetConfig().ServiceSettings.SiteURL
+	if ptr != nil {
+		siteURL = *ptr
+	}
+	return siteURL
 }
 
 func main() {
